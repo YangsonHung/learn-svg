529285b44ea8cebd95e0d54f038570917ceae71b
Yangson Hung <2545594421@qq.com>
2021-05-25 23:01:14 +0800

    [add]first commit

---
+++ src/utils/index.js
@@ -0,0 +1,17 @@
+export const changeDocumentTitle = () => {
+    var title = document.title;
+    document.addEventListener(
+        'visibilitychange',
+        function (event) {
+            if (document.hidden) {
+                document.title = '快回来~ (ಥ _ ಥ)';
+            } else {
+                document.title = 'hello~ (*^_^*)';
+                setTimeout(function () {
+                    document.title = title;
+                }, 2000);
+            }
+        },
+        false
+    );
+};
